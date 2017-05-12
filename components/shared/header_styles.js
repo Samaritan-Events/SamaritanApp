59142037e6cb3f519d3a8db9396d72cc20589df8
Josh Graber <josh.graber.85@gmail.com>
2017-05-12 09:32:40 -0400

    Add some initial component structure for main page
    Starting to scaffold the basic project structure for the application.
    Components will go in the components folder with a shared subfolder for
    layout / shared components. Underneath here components can be grouped by
    page hierarchy.
    
    Styles will be put next to the component they affect if possible or in
    the styles folder if they are shared across multiple components.
    
    We do need to figure out how to handle navigation across the app.

---
+++ components/shared/header_styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 28,
+  },
+})
+
+export default styles
---
+++ styles/layout.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  main: {
+    flex: 1,
+  },
+});
+
+export default styles
