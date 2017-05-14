2ef011b93b9b8147f12ecb07d9c48a4f7d2485d5
Josh Graber <josh.graber.85@gmail.com>
2017-05-14 09:34:20 -0400

    Add icons to the apps navbar
    The sizing and styling can be tweaked, but added icons for MyEvents,
    CreateEvent, and Profile.

--- components/shared/header_styles.js
+++ components/shared/header_styles.js
@@ -2,13 +2,24 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   header: {
-    height: 50,
-    justifyContent: 'center',
+    width: '100%',
+    height: 60,
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderTopColor: '#888',
+    borderBottomWidth: 2,
+    borderBottomColor: '#666',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
   headerText: {
     fontSize: 28,
   },
+  headerIcon: {
+    fontSize: 32,
+    color: '#080',
+  }
 })
 
 export default styles
